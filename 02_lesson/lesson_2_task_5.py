45eb0249e3d673694a3aca61f3fc0c686e05fe74
KApollinaria <korovkina39@yandex.ru>
2025-07-01 18:02:50 +0500

    Add files via upload

---
+++ 02_lesson/lesson_2_task_5.py
@@ -0,0 +1,19 @@
+def quarter_of_year(month):
+    if 1 <= month <= 2 or month == 12:
+        return "зима"
+    elif 3 <= month <= 5:
+        return "весна"
+    elif 6 <= month <= 8:
+        return "лето"
+    elif 9 <= month <= 11:
+        return "осень"
+    else:
+        return "Неверный номер месяца"
+
+
+try:
+    month = int(input("Введите номер месяца (1-12): "))
+    print(quarter_of_year(month))
+
+except ValueError:
+    print("Пожалуйста, введите целое число от 1 до 12.")
